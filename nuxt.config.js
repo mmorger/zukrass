b4cfb9ab2db5aced41a384b0b6bbe7ba1fd54cac
mmorger <mariusmorger@gmail.com>
2018-06-18 18:14:38 +0200

    [div] add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
    meta: [
      { charset: 'utf-8' },
      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-     { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+     { hid: 'description', name: 'description', content: 'Viele Jugendliche erleben körperliche, sexuelle oder psychische Gewalt. Die Kampagne «zukrass.ch» der Kantonalen Opferhilfestelle Zürich zeigt Opfern von Gewalt, wo sie kostenlose, vertrauliche und anonyme Hilfe finden.' },
+     { property: 'og:image', content: 'https://www.zukrass.ch/images/thumb_zukrass.png' },
      { name: 'msapplication-TileColor', content: '#ffffff' },
      { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
      { name: 'theme-color', content: '#ffffff' }
