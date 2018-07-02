75c0504166109a3a091414fbc94d9ae4d6ebda6e
mmorger <mariusmorger@gmail.com>
2018-07-02 11:34:22 +0200

    fix og-image

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,7 @@ module.exports = {
      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
      { hid: 'description', name: 'description', content: 'Viele Jugendliche erleben körperliche, sexuelle oder psychische Gewalt. Die Kampagne «zukrass.ch» der Kantonalen Opferhilfestelle Zürich zeigt Opfern von Gewalt, wo sie kostenlose, vertrauliche und anonyme Hilfe finden.' },
      { name: 'og:description', property: 'og:description', content: 'Viele Jugendliche erleben körperliche, sexuelle oder psychische Gewalt. Die Kampagne «zukrass.ch» der Kantonalen Opferhilfestelle Zürich zeigt Opfern von Gewalt, wo sie kostenlose, vertrauliche und anonyme Hilfe finden.' },
+     { name: 'og:image', property: 'og:image', content: 'https://www.zukrass.ch/images/thumb_zukrass.png' },
      { name: 'twitter:card', content: "summary" },
      { name: 'twitter:site', content: "@zukrassC" },
      { name: 'twitter:title', content: "#zukrass" },
